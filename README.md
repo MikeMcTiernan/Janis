0fe2151cd094ed4ddb31afa85b5479e687a6fe76
Michael McTiernan <mcipedia@gmail.com>
2011-06-02 19:04:57 -0700

    The birth of Janis.

---
+++ README.md
@@ -0,0 +1,81 @@
+Janis Javascript Animation Framework 0.1
+========================================
+
+Janis is a lightweight Javascript framework that provides simple animations via 
+CSS transitions for modern browsers on the web as well as mobile devices. It is
+not designed to solve everyone's problems--it's simply designed to organize 
+commonly used methods of animating DOM elements in Javascript.
+
+
+Usage
+----------------------------------------
+Janus uses a design pattern similar to jQuery--you may call it as a function,
+passing in the elements you wish to interact with and it will return a Janis
+object you can perform animations with. For instance:
+
+    var myElement = document.getElementById("somediv");
+    Janis(myElement).animate({
+        "left": "300px",
+        "duration": 1000
+    });
+    
+Will animate `<div id="somediv">` to an x position of 300 over 1000 ms. The first
+and only argument to the Janis() function can be an HTML Element, an array of 
+HTML Elements, a NodeList/HTMLCollection, or a CSS selector.
+
+Janis also supports chained animations. To run animations back to back in order:
+
+    var myAnimation = Janis(".my-div");
+    myAnimation.chain().animate({
+        "width": "100px",
+        "height": "100px",
+        "duration": 500
+    }).animate({
+        "width": "50px",
+        "height": "50px",
+        "duration": 500
+    });
+
+    // Execute the animation!
+    myAnimation.execute();
+
+This will animate any element with a class name of "my-div" to 100x100, followed by
+animating the matching elements to 50x50. If we wanted to, we could loop this
+chained animation by running the `loop()` method. The following syntax tells Janis 
+to loop our animation chain 10 times:
+
+    myAnimation.loop(10);
+
+Alternatively, we could set the chain to loop infinitely by passing -1 as the argument,
+or by simply leaving the argument undefined. We can also stop and start our animation at
+any time:
+
+    myAnimation.pause();
+    myAnimation.play();
+
+Please note, however, that the pause function does not stop the animation in
+place. The element will finish its current step before pausing. This is a current
+limitation in the CSS Transition specification, but I'm working out a clever 
+way to address this.
+
+
+Configuration and Defaults
+------------------------------------------
+This isn't the most elegant solution, but you can currently set default properties
+thusly:
+
+    Janis.config.duration = 1000; // Default 0
+    Janis.config.delay = 0; // Default 0
+    Janis.config.easing = "ease-in-out"; // Default 'linear'
+    Janis.config.browserProperty = 'WebkitTransition'; // Auto-detects by default
+
+
+Roadmap
+------------------------------------------
+*   Engineer method of pausing and resuming mid-transition
+*   Make demos and examples
+*   Develop better way (read: any way) of handling CSS Transforms
+*   Enable specifying functions to determine value of properties
+    ({"left": function(el) { return el.offsetLeft / 2; }})
+*   Enable relative properties ({"left": "+10px"})
+*   Dream up more features
---
+++ janis.js
@@ -0,0 +1,246 @@
+(function() {
+    if (!Object.prototype.forEach) Object.prototype.forEach = function(f, t) { 
+        for (var i in this) if (i !== "forEach") f.call(t, this[i], i, this);
+    };
+        
+    var Janis = function(target) {
+        if (target instanceof NodeList || target instanceof HTMLCollection) {
+            collection = new Array();
+            for (var i = 0; i < target.length; ++i) {
+                collection.push(target[i]);
+            }
+        } else if (typeof target == "string") {
+            collection = new Array();
+            tmp = document.querySelectorAll(target);
+            for (var i = 0; i < tmp.length; ++i) {
+                collection.push(tmp[i]);
+            }
+        } else if (!(target instanceof Array)) {
+            collection = new Array();
+            collection.push(target);
+        } else {
+            collection = target;
+        }
+        return new Janis.pt.init(collection);
+    };
+    
+    Janis.formatStyle = function(s) {
+        r = "";
+        s.split("-").forEach(function(v, i) {
+            r += i ? v.charAt(0).toUpperCase() + v.substr(1).toLowerCase() : v.toLowerCase();
+        });
+        return r;
+    };
+    
+    Janis.keys = function(o) {
+        var k = [];
+        for (var a in o) if (typeof o[a] !== "function") k.push(a);
+        return k;
+    };
+    
+    Janis.config = {
+        easing: "ease-in-out",
+        duration: 0,
+        delay: 0,
+        browserProperties: {
+            "transition" : "transition",
+            "MozTransition" : "-moz-transition",
+            "OTransition" : "-o-transition",
+            "webkitTransition" : "-webkit-transition",
+            "MsTransition" : "-ms-transition"
+        }
+    };
+    
+    Janis.pt = Janis.prototype = {
+        callbacks: {},
+        collection: [],
+        counter: 0,
+        init: function(collection) {
+            this.collection = collection;
+            if (typeof Janis.config.browserProperty === 'undefined') {
+                Janis.config.browserProperty = "transition";
+                var div = document.createElement('div'), style = "";
+                Janis.config.browserProperties.forEach(function(browserProperty) {
+                    style += browserProperty + ": width 1s linear;";
+                });
+                
+                div.setAttribute("style", style);
+                Janis.config.browserProperties.forEach(function(browserProperties, JSProp) {
+                    if (div.style[JSProp]) Janis.config.browserProperty = JSProp;
+                });
+            }
+            this.collection.forEach(function(el, idx) {
+                el.style[Janis.config.browserProperty + 'Property'] = "*";
+                el.style[Janis.config.browserProperty + 'Duration'] = Janis.config.duration + "ms";
+            });
+            return this;
+        },
+        _a: function(idx, opts) {
+            opts.index = ++this.counter;
+            if (opts.duration === undefined || opts.duration === null)
+                opts.duration = Janis.config.duration;
+            if (opts.delay === undefined || opts.delay === null)
+                opts.delay = Janis.config.delay;
+            if (opts.easing === undefined || opts.easing === null)
+                opts.easing = Janis.config.easing;
+            if (opts.callback === undefined || opts.callback === null)
+                opts.callback = function(janis, animation) {
+                    return;
+                };
+            if (!(opts.callback instanceof Array))
+                opts.callback = new Array(opts.callback);
+                
+            this.callbacks[opts['index']] = opts.callback;
+            
+            var el = this.collection[idx];
+            
+            el.style[Janis.config.browserProperty + "Duration"] = opts.duration + "ms";
+            el.style[Janis.config.browserProperty + "TimingFunction"] = opts.easing;
+        
+            var setStyles = function() {
+                for (var style in opts.css) {
+                    var value = opts.css[style];
+                    style = Janis.formatStyle(style);
+                    el.style[style] = value;
+                };
+            };
+            
+            if (!opts.delay) {
+                setStyles(opts);
+            } else {
+                window.setTimeout(setStyles, opts.delay);
+            }
+
+            var self = this;
+            window.setTimeout(function() {
+                for (var i in self.callbacks[opts['index']]) {
+                    if (typeof self.callbacks[opts['index']][i] == "function") {
+                        self.callbacks[opts['index']][i](self, opts);
+                    } else if (self.callbacks[opts['index']][i] instanceof Array) {
+                        self.callbacks[opts['index']][i][0].apply(self, self.callbacks[opts['index']][i][1]);
+                    }
+                }
+            }, opts.delay + opts.duration + 1);
+            
+            return this;
+        },
+        animate: function(animations, commit) {
+            if (!(animations instanceof Array)) {
+                var tmp = new Array();
+                for (var i in this.collection) {
+                    tmp[i] = animations;
+                }
+                animations = tmp;
+            }
+
+            if (this.isChained && !commit) {
+                this.chainQueue.push(["animate", [animations, true]]);
+                return this;
+            }
+
+            lastIdx = -1;
+            for (var idx in this.collection) {
+                config = {css: {}};
+                if (!animations[idx] && lastIdx === -1) return this;
+                if (!animations[idx]) {
+                    animation = animations[lastIdx];
+                } else {
+                    animation = animations[idx];
+                    lastIdx = idx;
+                }
+                
+                for (var key in animation) {
+                    if (key == "duration" || key == "delay" || key == "easing" || key == "callback") {
+                        config[key] = animation[key];
+                    } else {
+                        config.css[key] = animation[key];
+                    }
+                }
+                
+                this._a(idx, config);
+            }
+
+            return this;
+        },
+        maxDuration: function(animations) {
+            var maxDuration = -1,
+                totalTime = -1,
+                isTimeout = false;
+                
+            animations.forEach(function(el, idx) {
+                if (typeof el.duration == "undefined") el.duration = Janis.config.duration;
+                if (typeof el.delay == "undefined") el.delay = Janis.config.delay;
+                totalTime = (parseInt(el.duration) + parseInt(el.delay));
+                if ( totalTime > maxDuration ) {
+                    maxDuration = totalTime;
+                    isTimeout = true;
+                }
+            });
+            return maxDuration;
+        },
+        isChained: false,
+        chainQueue: [],
+        chain: function(/* void */) {
+            this.isChained = true;
+            this.chainQueue = [];
+            return this;
+        },
+        execute: function() {
+            if (!this.chainQueue.length) return this;
+            return this.loop(1);
+        },
+        _n: function(returnOperation) {
+            if (!this.chainQueue.length) return returnOperation ? [] : this;
+            var operation = this.chainQueue.shift(), self = this;
+            self[operation[0]].apply(self, operation[1]);
+            return returnOperation ? operation : this;
+        },
+        loopCounter: 0,
+        loopQueue: [],
+        loop: function(howMany, skipSetup) {
+            var self = this;
+            this.isChained = false;
+            if (!skipSetup) {
+                if (!this.chainQueue.length) return this;
+                if (typeof howMany == 'undefined') howMany = -1;
+                
+                this.loopCounter = parseInt(howMany);
+                this.loopQueue = [];
+                this.chainQueue.forEach(function(el, idx) {
+                    self.loopQueue.push(el);
+                });
+            }
+
+            if (!this.chainQueue.length && this.loopQueue.length) {
+                this.loopQueue.forEach(function(el) {
+                    self.chainQueue.push(el);
+                });
+                if (this.loopCounter > -1) --this.loopCounter;
+            }
+            if (this.loopCounter === 0 || !this.chainQueue.length) return this;            
+
+            if (!this.isPaused) {
+                var operation = this._n(true);
+                if (!operation.length) return;
+                var maxDuration = this.maxDuration(operation[1][0]);
+                window.setTimeout(function() {
+                    self.loop(self.loopCounter, true);
+                }, maxDuration);
+            }
+            return this;
+        },
+        isPaused: false,
+        pause: function() {
+            this.isPaused = true;
+            return this;
+        },
+        play: function() {
+            this.isPaused = false;
+            this.loop(this.loopCounter, true);
+            return this;
+        }
+    };
+    
+    Janis.pt.init.prototype = Janis.pt;
+    window['Janis'] = Janis;
+})();
